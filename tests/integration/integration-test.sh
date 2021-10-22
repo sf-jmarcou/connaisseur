d4f615881964d4761c62b9b60911404a76ddd6aa
Philipp Belitz <philipp.belitz@securesystems.de>
2021-10-22 11:04:38 +0200

    ci: Rework integration tests
    Reworked the integration tests, so that all logic is bundled into a single shell script file and all test cases in a yaml file. The upgrade test has been excluded.

--- tests/integration/integration-test.sh
+++ tests/integration/integration-test.sh
@@ -1,238 +1,305 @@
-#! /bin/bash
-set -euo pipefail
+set -eo pipefail
 
 # This script is expected to be called from the root folder of Connaisseur
-declare -i NUMBER_OF_VALID_DEPLOYMENTS=0
-declare -i NUMBER_OF_INVALID_DEPLOYMENTS=0
+declare -A DEPLOYMENT_VALIDITY=(["VALID"]="0" ["INVALID"]="0")
+RED="\033[0;31m"
+GREEN="\033[0;32m"
+NC="\033[0m"
+SUCCESS="${GREEN}SUCCESS${NC}"
+FAILED="${RED}FAILED${NC}"
+EXIT="0"
+WOLIST=("CronJob" "DaemonSet" "Deployment" "Job" "Pod" "ReplicaSet" "ReplicationController" "StatefulSet")
 
-echo 'Preparing Connaisseur config...'
-envsubst < tests/integration/update.yaml > update
-yq eval-all --inplace 'select(fileIndex == 0) * select(fileIndex == 1)' helm/values.yaml update
-rm update
-echo 'Config set'
+### SINGLE TEST CASE ####################################
+single_test() { # ID TXT TYP IMG NS OUT VAL
+  echo -n "[$1] $2"
+  if [[ "$3" == "deploy" ]]; then
+    kubectl run pod-$1 --image="$4" --namespace="$5" >output.log 2>&1 || true
+  elif [[ "$3" == "workload" ]]; then
+    envsubst <tests/integration/workload-objects/$4.yaml | kubectl apply -f - >output.log 2>&1 || true
+  else
+    kubectl apply -f $4 >output.log 2>&1 || true
+  fi
+  if [[ ! "$(cat output.log)" =~ "$6" ]]; then
+    echo -e ${FAILED}
+    echo "::group::Output"
+    cat output.log
+    kubectl logs -n connaisseur -lapp.kubernetes.io/instance=connaisseur
+    echo "::endgroup::"
+    EXIT="1"
+  else
+    echo -e "${SUCCESS}"
+  fi
 
-echo 'Installing Connaisseur...'
-# 'make' is chosen deliberately below to test the Makefile, while other tests use 'helm' directly
-make install || { echo 'Failed to install Connaisseur'; exit 1; }
-echo 'Successfully installed Connaisseur'
+  if [[ $7 != "null" ]]; then
+    DEPLOYMENT_VALIDITY[$7]=$((${DEPLOYMENT_VALIDITY[$7]} + 1))
+  fi
+}
 
-echo 'Testing unsigned image...'
-kubectl run pod --image=securesystemsengineering/testimage:unsigned >output.log 2>&1 || true
-NUMBER_OF_INVALID_DEPLOYMENTS+=1
+### MULTI TEST CASE FROM FILE ####################################
+multi_test() {
+  test_cases=$(yq e -o=json ".test_cases.$1" tests/integration/cases.yaml)
+  len=$(echo ${test_cases} | jq 'length')
+  for i in $(seq 0 $(($len - 1))); do
+    test_case=$(echo ${test_cases} | jq ".[$i]")
+    ID=$(echo ${test_case} | jq -r ".id")
+    TEST_CASE_TXT=$(echo ${test_case} | jq -r ".txt")
+    TYPE=$(echo ${test_case} | jq -r ".type")
+    REF=$(echo ${test_case} | jq -r ".ref")
+    NAMESPACE=$(echo ${test_case} | jq -r ".namespace")
+    MSG=$(echo ${test_case} | jq -r ".msg")
+    VALIDITY=$(echo ${test_case} | jq -r ".validity")
+    single_test "${ID}" "${TEST_CASE_TXT}" "${TYPE}" "${REF}" "${NAMESPACE}" "${MSG}" "${VALIDITY}"
+  done
+}
 
-if [[ ! "$(cat output.log)" =~ 'Unable to find signed digest for image docker.io/securesystemsengineering/testimage:unsigned.' ]]; then
-  echo 'Failed to deny unsigned image or failed with unexpected error. Output:'
-  cat output.log
+### WORKLOAD TEST ####################################
+workload_test() {
+  export KIND=$1
+  export APIVERSION=$(kubectl api-resources | awk -v KIND=${KIND} '{ if($NF == ""KIND"") print $(NF-2);}')
+
+  # UNSIGNED
+  export TAG=unsigned
+  echo "::group::${KIND}_${APIVERSION}_${TAG}.yaml"
+  envsubst <tests/integration/workload-objects/${KIND}.yaml | cat
+  echo "::endgroup::"
+  single_test "w_${KIND}_${APIVERSION}_${TAG}" "Testing ${KIND} using ${APIVERSION} and ${TAG} image..." "workload" "${KIND}" "deafult" "Unable to find signed digest for image docker.io/securesystemsengineering/testimage:unsigned." "null"
+
+  # SIGNED
+  export TAG=signed
+  echo "::group::${KIND}_${APIVERSION}_${TAG}.yaml"
+  envsubst <tests/integration/workload-objects/${KIND}.yaml | cat
+  echo "::endgroup::"
+  single_test "w_${KIND}_${APIVERSION}_${TAG}" "Testing ${KIND} using ${APIVERSION} and ${TAG} image..." "workload" "${KIND}" "deafult" " created" "null"
+}
+
+### STRESS TEST ####################################
+stress_test() {
+  NUMBER_OF_INSTANCES=100
+  echo -n 'Testing Connaisseur with complex requests...'
+  kubectl apply -f tests/integration/deployments/stresstest.yaml >output.log 2>&1 || true
+  if [[ ! ("$(cat output.log)" =~ 'deployment.apps/redis-with-many-instances created' && "$(cat output.log)" =~ 'pod/pod-with-many-containers created' && "$(cat output.log)" =~ 'pod/pod-with-many-containers-and-init-containers created' && "$(cat output.log)" =~ 'pod/pod-with-some-containers-and-init-containers created' && "$(cat output.log)" =~ 'pod/pod-with-coinciding-containers-and-init-containers created') ]]; then
+    echo -e ${FAILED}
+    echo "::group::Output"
+    cat output.log
+    echo "::endgroup::"
+    EXIT="1"
+  else
+    echo -e "${SUCCESS}"
+  fi
+
+  echo -n 'Cleaning up before second test...'
+  kubectl delete all -ltest=stresstest >/dev/null
+  echo -e "${SUCCESS}"
+
+  echo -n 'Testing Connaisseur with many requests...'
+  parallel --jobs 20 ./tests/integration/cause_load.sh {1} :::: <(seq ${NUMBER_OF_INSTANCES}) >output.log 2>&1 || true
+  NUMBER_CREATED=$(cat output.log | grep "deployment[.]apps/redis-[0-9]* created" | wc -l || echo "0")
+  if [[ ${NUMBER_CREATED} != "${NUMBER_OF_INSTANCES}" ]]; then
+    echo -e ${FAILED}
+    echo "::group::Output"
+    echo "Only ${NUMBER_CREATED}/${NUMBER_OF_INSTANCES} pods were created."
+    cat output.log
+    echo "::endgroup::"
+    EXIT="1"
+  else
+    echo -e "${SUCCESS}"
+  fi
+}
+
+### INSTALLING CONNAISSEUR ####################################
+install() {
+  echo -n "Installing Connaisseur..."
+  make install >/dev/null || {
+    echo -e "${FAILED}"
+    exit 1
+  }
+  echo -e "${SUCCESS}"
+}
+
+install_helm() {
+  echo -n "Installing Connaisseur..."
+  helm install connaisseur helm --atomic --create-namespace \
+    --namespace connaisseur >/dev/null || {
+    echo -e "${FAILED}"
+    exit 1
+  }
+  echo -e "${SUCCESS}"
+}
+
+### UPGRADING CONNAISSEUR ####################################
+upgrade() {
+  echo -n 'Upgrading Connaisseur...'
+  make upgrade >/dev/null || {
+    echo -e ${FAILED}
+    exit 1
+  }
+  echo -e "${SUCCESS}"
+}
+
+upgrade_helm() {
+  echo -n 'Upgrading Connaisseur...'
+  helm upgrade connaisseur helm -n connaisseur --wait >/dev/null || {
+    echo -e ${FAILED}
+    exit 1
+  }
+  echo -e "${SUCCESS}"
+}
+
+### UNINSTALING CONNAISSEUR ####################################
+uninstall() {
+  echo -n 'Uninstalling Connaisseur...'
+  make uninstall >/dev/null || {
+    echo -e "${FAILED}"
+    exit 1
+  }
+  echo -e "${SUCCESS}"
+}
+
+uninstall_helm() {
+  echo -n 'Uninstalling Connaisseur...'
+  helm uninstall connaisseur -n connaisseur &&
+    kubectl delete ns connaisseur >/dev/null || {
+    echo -e "${FAILED}"
+    exit 1
+  }
+  echo -e "${SUCCESS}"
+}
+
+update_values() {
+  for update in "$@"; do
+    yq e -i "${update}" helm/values.yaml
+  done
+}
+
+debug_vaules() {
+  echo "::group::values.yaml"
+  cat helm/values.yaml
+  echo "::endgroup::"
+}
+
+### RUN REGULAR INTEGRATION TEST ####################################
+regular_int_test() {
+  multi_test "regular"
+
+  ### EDGE CASE TAG IN RELEASES AND TARGETS ####################################
+  echo -n "[edge1] Testing edge case of tag defined in both targets and release json file..."
+  DEPLOYED_SHA=$(kubectl get pod pod-rs -o yaml | yq e '.spec.containers[0].image' - | sed 's/.*sha256://')
+  if [[ "${DEPLOYED_SHA}" != 'c5327b291d702719a26c6cf8cc93f72e7902df46547106a9930feda2c002a4a7' ]]; then
+    echo -e "${FAILED}"
+  else
+    echo -e "${SUCCESS}"
+  fi
+  N=$(($N + 1))
+
+  ### ALERTING TEST ####################################
+  echo -n "Checking whether alert endpoints have been called successfully..."
+  ENDPOINT_HITS="$(curl -s ${ALERTING_ENDPOINT_IP}:56243 --header 'Content-Type: application/json')"
+  NUMBER_OF_DEPLOYMENTS=$((${DEPLOYMENT_VALIDITY["VALID"]} + ${DEPLOYMENT_VALIDITY["INVALID"]}))
+  EXPECTED_ENDPOINT_HITS=$(jq -n \
+    --argjson REQUESTS_TO_SLACK_ENDPOINT ${NUMBER_OF_DEPLOYMENTS} \
+    --argjson REQUESTS_TO_OPSGENIE_ENDPOINT ${DEPLOYMENT_VALIDITY["VALID"]} \
+    --argjson REQUESTS_TO_KEYBASE_ENDPOINT ${DEPLOYMENT_VALIDITY["INVALID"]} \
+    '{
+  "successful_requests_to_slack_endpoint":$REQUESTS_TO_SLACK_ENDPOINT,
+  "successful_requests_to_opsgenie_endpoint": $REQUESTS_TO_OPSGENIE_ENDPOINT,
+  "successful_requests_to_keybase_endpoint": $REQUESTS_TO_KEYBASE_ENDPOINT
+  }')
+  diff <(echo "$ENDPOINT_HITS" | jq -S .) <(echo "$EXPECTED_ENDPOINT_HITS" | jq -S .) >diff.log 2>&1 || true
+  if [[ -s diff.log ]]; then
+    echo -e "${FAILED}"
+    echo "::group::Alerting endpoint diff:"
+    cat diff.log
+    echo "::endgroup::"
+    EXIT="1"
+  else
+    echo -e "${SUCCESS}"
+  fi
+}
+
+### COSIGN TEST ####################################
+cosign_int_test() {
+  multi_test "cosign"
+}
+
+### NAMESPACE VALIDATION TEST ####################################
+namespace_val_int_test() {
+  echo -n "Creating namespaces..."
+  kubectl create namespace ignoredns >/dev/null
+  kubectl label ns ignoredns securesystemsengineering.connaisseur/webhook=ignore >/dev/null
+  kubectl create namespace validatedns >/dev/null
+  kubectl label ns validatedns securesystemsengineering.connaisseur/webhook=validate >/dev/null
+  echo -e "${SUCCESS}"
+
+  multi_test "ignore-namespace-val"
+  update_values '.namespacedValidation.mode="validate"'
+  upgrade
+  multi_test "validate-namespace-val"
+}
+
+### DEPLOYMENT TEST ####################################
+deployment_int_test() {
+  multi_test "deployment"
+}
+
+### PRECONFIG TEST ####################################
+pre_config_int_test() {
+  multi_test "pre-config"
+}
+
+case $1 in
+"regular")
+  install
+  regular_int_test
+  uninstall
+  ;;
+"cosign")
+  install
+  cosign_int_test
+  ;;
+"namespace-val")
+  update_values '.namespacedValidation.enabled=true'
+  install
+  namespace_val_int_test
+  ;;
+"deployment")
+  update_values '.policy += {"pattern": "docker.io/library/*:*", "validator": "dockerhub-basics", "with": {"trust_root": "docker-official"}}'
+  install
+  deployment_int_test
+  ;;
+"pre-config")
+  install_helm
+  pre_config_int_test
+  uninstall_helm
+  ;;
+"workload")
+  install
+  pre_config_int_test
+  for wo in "${WOLIST[@]}"; do
+    workload_test "${wo}"
+  done
+  ;;
+"stress-test")
+  install
+  stress_test
+  ;;
+"all")
+  install
+  regular_int_test
+  cosign_int_test
+  update_values '.namespacedValidation.enabled=true'
+  upgrade
+  namespace_val_int_test
+  update_values '.namespacedValidation.enabled=false' '.policy += {"pattern": "docker.io/library/*:*", "validator": "dockerhub-basics", "with": {"trust_root": "docker-official"}}'
+  upgrade
+  deployment_int_test
+  uninstall
+  ;;
+*)
+  EXIT="1"
+  ;;
+esac
+
+if [[ $EXIT == "1" ]]; then
   exit 1
-else
-  echo 'Successfully denied usage of unsigned image'
 fi
-
-echo 'Testing image signed under different key...'
-kubectl run pod --image=library/redis >output.log 2>&1 || true
-NUMBER_OF_INVALID_DEPLOYMENTS+=1
-
-if [[ ! "$(cat output.log)" =~ 'Failed to verify signature of trust data root.' ]]; then
-  echo 'Failed to deny image signed with different key or failed with unexpected error. Output:'
-  cat output.log
-  exit 1
-else
-  echo 'Successfully denied usage of image signed under different key'
-fi
-
-echo 'Testing signed image...'
-kubectl run pod --image=securesystemsengineering/testimage:signed -lapp.kubernetes.io/instance=connaisseur >output.log 2>&1 || true
-NUMBER_OF_VALID_DEPLOYMENTS+=1
-
-if [[ "$(cat output.log)" != 'pod/pod created' ]]; then
-  echo 'Failed to allow signed image. Output:'
-  cat output.log
-  exit 1
-else
-  echo 'Successfully allowed usage of signed image'
-fi
-
-echo 'Testing edge case of tag defined in both targets and release json file...'
-DEPLOYED_SHA=$(kubectl get pod pod -o yaml | yq e '.spec.containers[0].image' - | sed 's/.*sha256://')
-if [[ "${DEPLOYED_SHA}" != 'c5327b291d702719a26c6cf8cc93f72e7902df46547106a9930feda2c002a4a7' ]]; then
-  echo "Connaisseur substituted wrong image: ${DEPLOYED_SHA}"
-  exit 1
-else
-  echo 'Connaisseur substituted correct image'
-fi
-
-echo 'Testing signed image with designated signer...'
-kubectl run pod2 --image=securesystemsengineering/testimage:special_sig -lapp.kubernetes.io/instance=connaisseur >output.log 2>&1 || true
-NUMBER_OF_VALID_DEPLOYMENTS+=1
-
-if [[ "$(cat output.log)" != 'pod/pod2 created' ]]; then
-  echo 'Failed to allow image signed by designated signer. Output:'
-  cat output.log
-  exit 1
-else
-  echo 'Successfully allowed usage of image signed by designated signer'
-fi
-
-echo 'Testing image with missing designated signer...'
-kubectl run pod3 --image=securesystemsengineering/testimage:wrong_signer >output.log 2>&1 || true
-NUMBER_OF_INVALID_DEPLOYMENTS+=1
-
-if [[ ! "$(cat output.log)" =~ 'Not all required delegations have trust data for image docker.io/securesystemsengineering/testimage:wrong_signer.' ]]; then
-  echo 'Failed to deny image with missing designated signer or failed with unexpected error. Output:'
-  cat output.log
-  exit 1
-else
-  echo 'Successfully denied usage of image with missing designated signer'
-fi
-
-echo 'Testing image with differing designated signers...'
-kubectl run pod3 --image=securesystemsengineering/testimage:double_sig >output.log 2>&1 || true
-NUMBER_OF_INVALID_DEPLOYMENTS+=1
-
-if [[ ! "$(cat output.log)" =~ 'Found multiple signed digests for image docker.io/securesystemsengineering/testimage:double_sig.' ]]; then
-  echo 'Failed to deny image with missing designated signer or failed with unexpected error. Output:'
-  cat output.log
-  exit 1
-else
-  echo 'Successfully denied usage of image with differing designated signers'
-fi
-
-echo 'Testing deployment of unsigned init container along with a valid container...'
-kubectl apply -f tests/integration/valid_container_with_unsigned_init_container_image.yml >output.log 2>&1 || true
-NUMBER_OF_INVALID_DEPLOYMENTS+=1
-
-if [[ ! "$(cat output.log)" =~ 'Unable to find signed digest for image docker.io/securesystemsengineering/testimage:unsigned.' ]]; then
-  echo 'Allowed an unsigned image via init container or failed due to an unexpected error handling init containers. Output:'
-  cat output.log
-  exit 1
-else
-  echo 'Successfully denied unsigned image in init container'
-fi
-
-echo 'Testing deployment of valid init container along with a valid container...'
-kubectl apply -f tests/integration/valid_init_container.yaml >output.log 2>&1 || true
-NUMBER_OF_VALID_DEPLOYMENTS+=1
-
-if [[ "$(cat output.log)" != 'pod/connaisseur-integration-test-pod-valid-init created' ]]; then
-  echo 'Failed to deploy a valid initContainer along with a valid container. Output:'
-  cat output.log
-  exit 1
-else
-  echo 'Successfully allowed valid image in init container and container'
-fi
-
-echo 'Checking whether alert endpoints have been called successfully'
-ENDPOINT_HITS=$(curl ${ALERTING_ENDPOINT_IP}:56243 --header "Content-Type: application/json")
-let NUMBER_OF_DEPLOYMENTS=${NUMBER_OF_INVALID_DEPLOYMENTS}+${NUMBER_OF_VALID_DEPLOYMENTS}
-EXPECTED_ENDPOINT_HITS=$(jq -n \
---argjson REQUESTS_TO_SLACK_ENDPOINT ${NUMBER_OF_DEPLOYMENTS} \
---argjson REQUESTS_TO_OPSGENIE_ENDPOINT  ${NUMBER_OF_VALID_DEPLOYMENTS} \
---argjson REQUESTS_TO_KEYBASE_ENDPOINT ${NUMBER_OF_INVALID_DEPLOYMENTS} \
-'{
-"successful_requests_to_slack_endpoint":$REQUESTS_TO_SLACK_ENDPOINT,
-"successful_requests_to_opsgenie_endpoint": $REQUESTS_TO_OPSGENIE_ENDPOINT,
-"successful_requests_to_keybase_endpoint": $REQUESTS_TO_KEYBASE_ENDPOINT
-}')
-echo "Hit the alerting endpoints ${ENDPOINT_HITS} times; expected was ${EXPECTED_ENDPOINT_HITS}."
-diff <(echo $ENDPOINT_HITS | jq -S .) <(echo $EXPECTED_ENDPOINT_HITS | jq -S .) >output.log 2>&1
-if [[ -s output.log ]]; then
-  cat output.log
-  exit 1
-else
-  echo 'Successfully called mocked alert endpoints'
-fi
-
-echo 'Creating test namespaces'
-kubectl create namespace ignoredns
-kubectl label ns ignoredns securesystemsengineering.connaisseur/webhook=ignore
-kubectl create namespace validatedns
-kubectl label ns validatedns securesystemsengineering.connaisseur/webhook=validate
-
-echo '### Testing "ignore" label ###'
-
-echo 'Upgrading Connaisseur...'
-yq eval-all --inplace 'select(fileIndex == 0) * select(fileIndex == 1)' helm/values.yaml tests/integration/namespaced-update.yaml
-make upgrade || { echo 'Failed to upgrade Connaisseur'; exit 1; }
-echo 'Successfully upgraded Connaisseur'
-
-echo 'Testing unsigned image in unlabelled namespace...'
-kubectl run pod --namespace connaisseur --image=securesystemsengineering/testimage:unsigned >output.log 2>&1 || true
-
-if [[ ! "$(cat output.log)" =~ 'Unable to find signed digest for image docker.io/securesystemsengineering/testimage:unsigned.' ]]; then
-  echo 'Failed to deny unsigned image or failed with unexpected error. Output:'
-  cat output.log
-  exit 1
-else
-  echo 'Successfully denied usage of unsigned image'
-fi
-
-echo 'Testing signed image in unlabelled namespace...'
-kubectl run pod --namespace connaisseur --image=securesystemsengineering/testimage:signed -lapp.kubernetes.io/instance=connaisseur >output.log 2>&1 || true
-
-if [[ "$(cat output.log)" != 'pod/pod created' ]]; then
-  echo 'Failed to allow signed image. Output:'
-  cat output.log
-  exit 1
-else
-  echo 'Successfully allowed usage of signed image'
-fi
-
-echo 'Testing unsigned image in ignored namespace...'
-kubectl run pod --namespace ignoredns --image=securesystemsengineering/testimage:unsigned -lapp.kubernetes.io/instance=connaisseur >output.log 2>&1 || true
-
-if [[ "$(cat output.log)" != 'pod/pod created' ]]; then
-  echo 'Failed to allow unsigned image in ignored namespace. Output:'
-  cat output.log
-  exit 1
-else
-  echo 'Successfully allowed usage of unsigned image in ignored namespace'
-fi
-
-echo '### Testing "validate" label ###'
-
-yq e '.namespacedValidation.mode="validate"' -i "helm/values.yaml"
-
-echo 'Upgrading Connaisseur again...'
-helm upgrade connaisseur helm --wait --namespace connaisseur || { echo 'Failed to upgrade Connaisseur'; exit 1; }
-echo 'Successfully upgrade Connaisseur again'
-
-echo 'Testing unsigned image in enabled namespace...'
-kubectl run pod --namespace validatedns --image=securesystemsengineering/testimage:unsigned >output.log 2>&1 || true
-
-if [[ ! "$(cat output.log)" =~ 'Unable to find signed digest for image docker.io/securesystemsengineering/testimage:unsigned.' ]]; then
-  echo 'Failed to deny unsigned image or failed with unexpected error. Output:'
-  cat output.log
-  exit 1
-else
-  echo 'Successfully denied usage of unsigned image'
-fi
-
-echo 'Testing signed image in enabled namespace...'
-kubectl run pod --namespace validatedns --image=securesystemsengineering/testimage:signed -lapp.kubernetes.io/instance=connaisseur >output.log 2>&1 || true
-
-if [[ "$(cat output.log)" != 'pod/pod created' ]]; then
-  echo 'Failed to allow signed image. Output:'
-  cat output.log
-  exit 1
-else
-  echo 'Successfully allowed usage of signed image'
-fi
-
-echo 'Testing unsigned image in unlabelled namespace...'
-kubectl run pod2 --namespace connaisseur --image=securesystemsengineering/testimage:unsigned -lapp.kubernetes.io/instance=connaisseur >output.log 2>&1 || true
-
-if [[ "$(cat output.log)" != 'pod/pod2 created' ]]; then
-  echo 'Failed to allow unsigned image in ignored namespace. Output:'
-  cat output.log
-  exit 1
-else
-  echo 'Successfully allowed usage of unsigned image in ignored namespace'
-fi
-
-echo 'Uninstalling Connaisseur...'
-# 'make' is chosen deliberately below to test the Makefile, while other tests use 'helm' directly
-make uninstall || { echo 'Failed to uninstall Connaisseur'; exit 1; }
-echo 'Successfully uninstalled Connaisseur'
-
-rm output.log
-echo 'Passed integration test'
